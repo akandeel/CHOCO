3e027e87fed29a7008b6324f90761b9454834ac5
your github.com username <kandeel.a1@gmail.com>
2017-06-07 21:20:09 -0400

    rebase master to working branch. there is a now a new sessions_consumer controller

---
+++ app/helpers/sessions_consumers_helper.rb
@@ -0,0 +1,2 @@
+module SessionsConsumersHelper
+end
---
+++ test/controllers/sessions_consumers_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SessionsConsumersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
