7ba488ac541a900667e5711644fdf3ac76477e8b
your github.com username <kandeel.a1@gmail.com>
2017-06-15 21:47:50 -0400

    create validations for consumer.rb

--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -2,5 +2,21 @@ class Consumer < ApplicationRecord
 
   has_many :sales
 
+  validates_presence_of :first_name,
+                        :last_name,
+                        :gender,
+                        :date_of_birth,
+                        :country,
+                        :street_number,
+                        :street_name,
+                        :state,
+                        :suit,
+                        :mailing_address,
+                        :email_address
+
+  validates_uniqueness_of :first_name,
+                          :last_name,
+                          :email_address
+
 
 end
