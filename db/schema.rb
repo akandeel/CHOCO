fa93205decd0822cb7a50d2ab6281b8c7b0c8aba
your github.com username <kandeel.a1@gmail.com>
2017-06-27 19:27:44 -0400

    rails db:migrate for remember_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615235624) do
+ActiveRecord::Schema.define(version: 20170627231704) do
 
   create_table "businesses", force: :cascade do |t|
     t.string   "business_name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170615235624) do
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
     t.string   "password_confirmation"
+    t.string   "remember_digest"
   end
 
   create_table "sessions", force: :cascade do |t|
