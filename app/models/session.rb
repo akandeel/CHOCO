90c863a5197247d1d2509815c770d25b366e4f23
your github.com username <kandeel.a1@gmail.com>
2017-06-27 21:49:07 -0400

    add methods new, create and destroy with remember and log_in helpers to be used in helpers/sessions_helper.rb

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,2 +1,33 @@
 class Session < ApplicationRecord
+
+  def new
+  end
+
+  def create
+    consumer = Consumer.find_by(email: params[:session][:email].downcase)
+     if consumer && consumer.authenticate(params[:session][:password])
+       log_in user
+       remember consumer #this is the remember helper
+       redirect_to consumer
+
+       #defers the real work to the Sessions
+       #helper, where we define a remember
+       #method that calls user.remember, thereby
+       #generating a remember token and saving its
+       # digest to the database. It then uses cookies
+       #to create permanent cookies for the user id
+       #and remember token as described above.
+       #The result appears in
+       #app/helpers/sessions_consumers_helper.rb
+
+     else
+       flash.now[:danger] = 'Invalid email or password'
+       render 'new'
+     end
+   end
+
+   def destroy
+     log_out
+     redirect_to root_url
+   end
 end
