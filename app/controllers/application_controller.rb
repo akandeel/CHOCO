a96248e3c65ddc4ea91440b6f0dd60ecbf09485c
your github.com username <kandeel.a1@gmail.com>
2017-06-28 00:03:18 -0400

    add SessionsHelper module to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  include SessionsHelpers
+
 
 private
   #once consumer is logged in he/she should be
