6a57eb512cf308151e0f6e09743eb7876a9714a9
your github.com username <kandeel.a1@gmail.com>
2017-06-29 22:37:48 -0400

    add logged_in? method to sessions_consumers_helper.rb

--- app/helpers/sessions_consumers_helper.rb
+++ app/helpers/sessions_consumers_helper.rb
@@ -9,11 +9,30 @@ module SessionsConsumersHelper
 
     # Logs in the given consumer.
   def log_in(consumer)
-    session[:consumer.id] = consumer.id
+    session[:consumer.id] = consumer.id #session[:] is provided from rails.
   end
 
   # Returns the current logged-in user (if any).
+  ####check in console with a non existant record
+  ####that this raises no exception and returns nil if id is invalid.
   def current_consumer
-    @current_consumer ||= User.find_by(id: session[:consumer_id])
+    #if @current_consumer.nil?
+    #   @current_consumer = User.find_by(id: session[:user_id])
+    #else
+    #   @current_consumer
+    #end
+
+    #***** THIS LINE DOES THE SAME *****
+    @current_consumer ||= Consumer.find_by(id: session[:consumer_id])
   end
+
+  #A user is logged in if there is a current
+  #user in the session, i.e.,
+  #if current_user is not nil.
+
+  ##### Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_consumer?
+  end
+
 end
