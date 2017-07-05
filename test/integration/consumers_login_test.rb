15e1d98ff69a89c14d37d9e7a52c129235f78f59
your github.com username <kandeel.a1@gmail.com>
2017-07-04 21:18:53 -0400

    add notes to tests

--- test/integration/consumers_login_test.rb
+++ test/integration/consumers_login_test.rb
@@ -23,10 +23,10 @@ class ConsumersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     post login_path, params: { session: { email:    @consumer.email,
                                           password: 'password' } }
-    assert_redirected_to @consumer
-    follow_redirect!
+    assert_redirected_to @consumer #to check the right redirect target
+    follow_redirect! #to actually visit the target page.
     assert_template 'consumers/show'
-    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", login_path, count: 0 #we expect there to be zero links matching the given pattern.
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", consumer_path(@consumer)
   end
