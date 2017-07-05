a1b9f1f2d27f1aff2fc3f0884da521c079cc0c78
your github.com username <kandeel.a1@gmail.com>
2017-07-04 20:44:31 -0400

    add password_digest to consumer.rb for custom fixture we created for login

--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -18,5 +18,35 @@ class Consumer < ApplicationRecord
                           :last_name,
                           :email_address
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+has_secure_password
+
+#don't need to validate :password,
+#presence because has_secure_password
+#already does it.
+
+# Returns the hash digest of the given string.
+# a method to create a password_digest attribute for our custom fixture consumer.
+# *** the password digest is created using bcrypt (via has_secure_password)
+  def Consumer.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  #Because we’ll need to log the user in, we also
+  #have to include a valid password to compare
+  #with the password submitted to the Sessions
+  #controller’s create action. Referring to the
+  #model we see that this
+  #means creating a password_digest attribute
+  #for the user fixture, which we’ll accomplish
+  #by defining a digest method of our own.
+
+
 
 end
