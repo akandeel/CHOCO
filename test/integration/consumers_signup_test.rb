1ea70e0c51efc0fec81625faa3037300748c48c1
your github.com username <kandeel.a1@gmail.com>
2017-07-04 22:35:40 -0400

    add testing for consumers_signup_test

--- test/integration/consumers_signup_test.rb
+++ test/integration/consumers_signup_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ConsumersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+   test "valid signup information" do
+     get signup_path
+     assert_difference 'Consumer.count', 1 do
+    post users_path, params: { user: { name:  "Example User",
+                                          email: "consumer@example.com",
+                                          password:              "password",
+                                          password_confirmation: "password" } }
+   end
+     follow_redirect! #to check acctual page.
+     assert_template 'users/show'
+     assert is_logged_in? #created test_helper and is a boolean check.
+   end
 end
