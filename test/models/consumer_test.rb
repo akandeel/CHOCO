b40b23357169bc9de8a2edea8c95479160fdacff
your github.com username <kandeel.a1@gmail.com>
2017-07-05 21:49:14 -0400

    fix consumer_test.rb

--- test/models/consumer_test.rb
+++ test/models/consumer_test.rb
@@ -5,10 +5,8 @@ class ConsumerTest < ActiveSupport::TestCase
   def setup
     @consumer = Consumer.new(name: "Chocolate Lover", email: "choco@example.com",
                      password: "foobar", password_confirmation: "foobar")
-  en
-  .
-  .
-  .
+  end
+
   test "authenticated? should return false for a consumer with nil digest" do
     assert_not @consumer.authenticated?('')
   end
