ed4397f419635b24bfa0b263188e37bec7f7433f
your github.com username <kandeel.a1@gmail.com>
2017-07-06 22:24:26 -0400

    add test integration login without remembering

--- test/integration/consumers_login_test.rb
+++ test/integration/consumers_login_test.rb
@@ -9,11 +9,11 @@ class ConsumersLoginTest < ActionDispatch::IntegrationTest
 
   # ***** ENSURE THAT FLASH MESSAGE DISAPPEARS ON NEW PAGE *****
   test "login with invalid information" do
-  get login_path
-  post login_path, params: { session: {email: "", password: ""}}
+    get login_path
+    post login_path, params: { session: {email: "", password: ""}}
 
-  assert_template 'sessions/new'
-  assert_template 'sessions/new'
+    assert_template 'sessions/new'
+    assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
@@ -32,24 +32,37 @@ class ConsumersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test "login with valid information followed by logout" do
-  get login_path
-  post login_path, params: { session: { email:    @consumer.email,
-                                        password: 'password' } }
-  assert is_logged_in? # also tests that this happens immediately after posting valid information to sessions path.
-  assert_redirected_to @consumer
-  follow_redirect!
-  assert_template 'consumers/show'
-  assert_select "a[href=?]", login_path, count: 0
-  assert_select "a[href=?]", logout_path
-  assert_select "a[href=?]", consumer_path(@consumer)
-  delete logout_path
-  assert_not is_logged_in?
-  assert_redirected_to root_url
+    get login_path
+    post login_path, params: { session: { email:    @consumer.email,
+                                          password: 'password' } }
+    assert is_logged_in? # also tests that this happens immediately after posting valid information to sessions path.
+    assert_redirected_to @consumer
+    follow_redirect!
+    assert_template 'consumers/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", consumer_path(@consumer)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
 
-  delete logout_path # Simulate a consumer clicking logout in a second window.
-  follow_redirect!
-  assert_select "a[href=?]", login_path
-  assert_select "a[href=?]", logout_path,      count: 0
-  assert_select "a[href=?]", consumer_path(@consumer), count: 0
-end
+    delete logout_path # Simulate a consumer clicking logout in a second window.
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", consumer_path(@consumer), count: 0
+  end
+
+  test "login with remembering" do
+   log_in_as(@consumer, remember_me: '1')
+   assert_not_empty cookies['remember_token']
+  end
+
+  test "login without remembering" do
+   # Log in to set the cookie.
+   log_in_as(@consumer, remember_me: '1')
+   # Log in again and verify that the cookie is deleted.
+   log_in_as(@consumer, remember_me: '0')
+   assert_empty cookies['remember_token']
+ end
 end
