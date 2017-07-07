01539fc43fc7bcfff628871e36f5508c3d20dc9a
your github.com username <kandeel.a1@gmail.com>
2017-07-06 22:54:02 -0400

    add def log_in_as in integration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -26,8 +26,34 @@ class ActiveSupport::TestCase
   end
 
   # Log in as a particular user.
+  #Inside integration tests, we can’t
+  #manipulate session directly, but we
+  #can post to the sessions path
+  #which leads to the log_in_as
  def log_in_as(user)
    session[:user_id] = user.id
  end
- 
+
+
+
+ #Because it’s located inside the ActionDispatch::IntegrationTest
+ #class, this is the version of log_in_as that will
+ #be called inside integration tests. We use the
+ #same method name in both cases because it lets
+ #us do things like use code from a controller
+ #test in an integration without making any
+ #changes to the login method.
+
+#Putting these two methods together yields the parallel log_in_as helpers
+ class ActionDispatch::IntegrationTest
+
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
+
+  # IN CONSUMERS_LOGIN_TEST.RB WE WILL USE THIS TO WRITE 2 TESTS FOR THE REMEMBER ME!
+
 end
