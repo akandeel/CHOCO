5498525ff111a518e906f646875c4b990c6e6274
your github.com username <kandeel.a1@gmail.com>
2017-07-11 21:59:55 -0400

    add 2 tests to consumer_test.rb password should be presenet and password should have min length

--- test/models/consumer_test.rb
+++ test/models/consumer_test.rb
@@ -10,6 +10,16 @@ class ConsumerTest < ActiveSupport::TestCase
                              assword_confirmation: "foobar")
   end
 
+  test "password should be present (nonblank)" do
+    @consumer.password = @consumer.password_confirmation = " " * 6
+    assert_not @consumer.valid?
+  end
+
+  test "password should have a minimum length" do
+    @consumer.password = @consumer.password_confirmation = "a" * 5
+    assert_not @consumer.valid?
+  end
+
   #this is easier to test in model rather than integration. which is why its here.
   test "authenticated? method. It should return false for a consumer with nil digest" do
     assert_not @consumer.authenticated?('')
