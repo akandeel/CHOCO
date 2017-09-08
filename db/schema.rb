2d01a6d086ff9d8eb7dd6227c7ff833c972f4ed0
your github.com username <kandeel.a1@gmail.com>
2017-09-08 00:29:40 -0400

    migrate:up sessions migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170607233010) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
