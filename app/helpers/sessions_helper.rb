6684e6eaec9799c36df5e8eaab6c3912d005fc25
your github.com username <kandeel.a1@gmail.com>
2017-10-31 22:18:21 -0400

    refactor sessions_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -62,7 +62,7 @@ module SessionsHelper
     elsif (user_id = cookies.signed[:user_id])
       #raise # raise an exception in the suspected untested block of code: if the code isn’t covered, the tests will still pass; if it is covered, the resulting error will identify the relevant test
       user = User.find_by(id: user_id)
-        if user && user.authenticated?(cookies[:remember_token])
+        if user && user.authenticated?(:remember, cookies[:remember_token])
           log_in user
           @current_user = user
         end
