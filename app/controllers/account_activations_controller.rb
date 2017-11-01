67193c8417d148132993f610333cf4522173697f
your github.com username <kandeel.a1@gmail.com>
2017-10-31 22:35:22 -0400

    def edit action. activate, athenticate, login, then back to user. else back to home page

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,6 +2,15 @@ class AccountActivationsController < ApplicationController
 
 
   def edit
+    user = User.find_by(email_address: params[:email_address])
+    if user && !user.activated? && user.authenticated?(:activation, params([:id]) )
+      user.update_attribute(:activated, true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      redirect_to users_path
+    else
+      redirect_to root_url
+    end
   end
 
 end
