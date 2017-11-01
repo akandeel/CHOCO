23c205f44a600fe746b717dd56106af78201b8c0
your github.com username <kandeel.a1@gmail.com>
2017-10-31 22:06:58 -0400

    refactor def authenticated? to despecialize it on remember_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -105,6 +105,7 @@ has_secure_password
    #USING BCRYPT CODE
    # Returns true if the given token matches the digest.
    def authenticated?(remember_token)
+     digest = self.send("remember_digest")
      return false if remember_digest.nil?
     #if remember_digest.nil? # using nil and returning false fixes error in test.
      #false
