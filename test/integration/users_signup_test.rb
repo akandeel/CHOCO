8112a97be1ab888633d080f9c8ffb6ff3e383119
your github.com username <kandeel.a1@gmail.com>
2017-11-02 22:07:14 -0400

    add several other tests. mostly assert_nots

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -19,13 +19,29 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             }
 end
 
-     follow_redirect!
      #We’ve also included a call
      #to assert_template to check
      #that a failed submission re-renders the new action.
     # assert is_logged_in? #created test_helper and is a boolean check.
+    follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
-    
+#the first few assertions are important to the app. the following tests are trivial.
+
+    asser_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+
+# the following tests are to try to log in before activation.
+    log_in as(user)
+    assert_not user.activated?
+
+#these tests are to try to send activation email with wrong activation token.
+    get edit_account_activation_path(user.activation_token, email_address: 'wrong')
+    assert_not is_logged_in?
+#Now do the same with valid activation token.
+    get edit_account_acitvation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+
    end
 end
